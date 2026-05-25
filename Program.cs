40bfe5576976de5371c9400be39d370a9182a18a
user <t1135537@gmail.com>
2026-05-25 23:30:21 +0300

    firebase fix

--- Program.cs
+++ Program.cs
@@ -8,21 +8,17 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ---------- ПОРТ 8080 (Amvera) ----------
 builder.WebHost.UseUrls("http://*:8080");
 
-// ---------- СЕРВИСЫ ----------
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Подключение БД (строка берётся из переменной окружения)
 var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
     ?? builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// Инициализация Firebase Admin (из переменной окружения)
 var firebaseJson = Environment.GetEnvironmentVariable("FIREBASE_ADMIN_SDK_JSON");
 if (!string.IsNullOrEmpty(firebaseJson))
 {
@@ -33,14 +29,12 @@ if (!string.IsNullOrEmpty(firebaseJson))
 }
 else
 {
-    // fallback для локальной разработки
     FirebaseApp.Create(new AppOptions
     {
         Credential = GoogleCredential.FromFile("firebase-admin-sdk.json")
     });
 }
 
-// JWT-аутентификация (Firebase)
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
